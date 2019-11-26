2b1aee6dcc41dd0986aced3bb7e56a6acefd3350
Daniele Saitta <d.saitta@reply.it>
2019-11-26 23:30:28 +0100

    Added license, updated manifest

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.dynatraceMessageReader', () => {
+	let disposable = vscode.commands.registerCommand('extension.dynMessageReader', () => {
 		// Get the active text editor
 		let editor = vscode.window.activeTextEditor;
 
